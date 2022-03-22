d3f228e4353684debc79faf407a2b80c02a001e5
Alexander Novikov <anovikov@tsum.ru>
2022-03-22 17:45:47 +0300

    ref fix

--- src/main/scala/memmemov/clicon/Server.scala
+++ src/main/scala/memmemov/clicon/Server.scala
@@ -26,48 +26,46 @@ object Server extends IOApp {
     val seconds: Stream[IO, FiniteDuration] = Stream.awakeEvery[IO](100.millis)
 
     case class Connection(from: Option[EntityBody[IO]])
-    val connectionRef: IO[Ref[IO, Connection]] = Ref[IO].of(Connection(None))
+    val connectionRefIO: IO[Ref[IO, Connection]] = Ref[IO].of(Connection(None))
 
-    def directorRoutes: HttpRoutes[IO] = {
+    def directorRoutes(connectionRef: Ref[IO, Connection]): HttpRoutes[IO] = {
       val dsl = Http4sDsl[IO]
       import dsl._
 
       HttpRoutes.of[IO] {
         case req @ POST -> Root / "from" =>
           for {
-            _ <- IO(println("from stream acquired"))
-            connection <- connectionRef
-            _ <- connection.update(_ => Connection(Option(req.body)))
-            result <- Ok(seconds.map(_.toString))
+            _ <- IO(println("acquiring from stream..."))
+            connection <- connectionRef.updateAndGet(_ => Connection(Option(req.body)))
+            _ <- IO(println(s"from stream acquired $connection"))
+            result <- IO.sleep(10.second) >> Ok(req.body)
           } yield result
-//          Ok(seconds.map(_.toString))
-
 
         case req @ POST -> Root / "to" =>
           for {
-            connection <- connectionRef
-            c <- connection.get
-            result <- Ok(c.from match {
-              case None => seconds.map(_.toString)
+            _ <- IO(println("opening to..."))
+            connection <- connectionRef.get
+            _ <- IO(println(s"got connection $connection"))
+            result <- Ok(connection.from match {
+              case None => seconds.map(_ => "NOTHING ")
               case Some(bodyStream) => bodyStream.map(_.toString)
             })
           } yield result
       }
     }
 
-    def testCleartext =
+    def testCleartext(connectionRef: Ref[IO, Connection]) =
       EmberServerBuilder
         .default[IO]
         .withHttp2
         .withHost(ipv4"0.0.0.0")
         .withPort(port"8080")
-        .withHttpApp(directorRoutes.orNotFound)
+        .withHttpApp(directorRoutes(connectionRef).orNotFound)
         .build
   }
 
-  def run(args: List[String]): IO[ExitCode] =
-    ServerTest
-      .testCleartext
-      .use(_ => IO.never)
-      .as(ExitCode.Success)
+  def run(args: List[String]): IO[ExitCode] = (for {
+    connectionRef <- ServerTest.connectionRefIO
+    code <- ServerTest.testCleartext(connectionRef).use(_ => IO.never).as(ExitCode.Success)
+  } yield code)
 }
