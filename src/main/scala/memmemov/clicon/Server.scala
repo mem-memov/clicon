bb71218ac0d34d32e8ea3d5d02dd6e09aeb20469
Alexander Novikov <anovikov@tsum.ru>
2022-03-30 18:00:48 +0300

    one way stream

--- src/main/scala/memmemov/clicon/Server.scala
+++ src/main/scala/memmemov/clicon/Server.scala
@@ -29,7 +29,7 @@ object Server extends IOApp {
 
   object ServerTest {
 
-    val seconds: Stream[IO, FiniteDuration] = Stream.awakeEvery[IO](100.millis)
+    val seconds: Stream[IO, FiniteDuration] = Stream.awakeEvery[IO](10.millis)
 
     case class Connection(from: Option[EntityBody[IO]])
     val connectionRefIO: IO[Ref[IO, Connection]] = Ref[IO].of(Connection(None))
@@ -40,7 +40,9 @@ object Server extends IOApp {
       connectionRef: Ref[IO, Connection], 
       transmissionRef: Ref[IO, R[symbol.Transmission]],
       forwardQueue: Queue[IO, Option[Byte]],
-      forwardStream: Stream[IO, Byte]
+      forwardStream: Stream[IO, Byte],
+      backwardQueue: Queue[IO, Option[Byte]],
+      backwardStream: Stream[IO, Byte]
     ): HttpRoutes[IO] = {
       val dsl = Http4sDsl[IO]
       import dsl._
@@ -52,32 +54,45 @@ object Server extends IOApp {
             newT <- IO(plug(t, StreamSymbol(Stream.never), StreamSymbol(req.body)))
             _ <- transmissionRef.update(_ => newT)
 
-            _ <- IO(req.body.drain.map(byte => Some(byte)).map(forwardQueue.offer))
+            _ <- IO(println("FROM started"))
+            fw <- IO(req.body.map(byte => Option(byte)).evalTap{ ob =>
+              for {
+                _ <- forwardQueue.offer(ob)
+                _ <- IO(println(s"[from] $ob"))
+              } yield ()
+            }.collect {
+              case Some(v) => v
+            })
 
-            result <- IO.sleep(5.second) >> Ok(req.body)
+            result <- Ok(fw)
 
 //            connection <- connectionRef.updateAndGet(_ => Connection(Option(req.body)))
 //            result <- IO.sleep(10.second) >> Ok(req.body)
           } yield result
 
         case req @ POST -> Root / "to" =>
-
-//          for {
+          for {
 //            t <- transmissionRef.get
 //            newT <- IO(plug(t, StreamSymbol(Stream.never), StreamSymbol(req.body)))
 //            _ <- transmissionRef.update(_ => newT)
 
-//            _ <- IO(println(forwardStream))
-//            result <- Ok(forwardStream)
-//            result <- Ok(seconds.map(_ => "NOTHING "))
+            _ <- IO(println("TO started"))
+//            _ <- IO(req.body.map(byte => Option(byte)).evalTap{ ob =>
+//              for {
+//                _ <- backwardQueue.offer(ob)
+//                _ <- IO(println(s"[to] $ob"))
+//              } yield ()
+//            })
+
+            result <- Ok(forwardStream)
 
 //            connection <- connectionRef.get
 //            result <- connection.from match {
 //              case None => Ok(seconds.map(_ => "NOTHING "))
 //              case Some(bodyStream) => Ok(bodyStream)
 //            }
-//          } yield ()
-          Ok(seconds.map(_ => "NOTHING "))
+          } yield result
+
       }
     }
 
@@ -87,14 +102,23 @@ object Server extends IOApp {
       connectionRef: Ref[IO, Connection],
       transmissionRef: Ref[IO, R[symbol.Transmission]],
       forwardQueue: Queue[IO, Option[Byte]],
-      forwardStream: Stream[IO, Byte]
+      forwardStream: Stream[IO, Byte],
+      backwardQueue: Queue[IO, Option[Byte]],
+      backwardStream: Stream[IO, Byte]
     ) =
       EmberServerBuilder
         .default[IO]
         .withHttp2
         .withHost(ipv4"0.0.0.0")
         .withPort(port"8080")
-        .withHttpApp(buildRoutes(connectionRef, transmissionRef, forwardQueue, forwardStream).orNotFound)
+        .withHttpApp(buildRoutes(
+          connectionRef,
+          transmissionRef,
+          forwardQueue,
+          forwardStream,
+          backwardQueue,
+          backwardStream
+        ).orNotFound)
         .build
   }
 
@@ -132,8 +156,17 @@ object Server extends IOApp {
 
       forwardQueue <- Queue.unbounded[IO, Option[Byte]]
       forwardStream <- IO(Stream.fromQueueNoneTerminated(forwardQueue))
+      backwardQueue <- Queue.unbounded[IO, Option[Byte]]
+      backwardStream <- IO(Stream.fromQueueNoneTerminated(forwardQueue))
 
       connectionRef <- ServerTest.connectionRefIO
-      code <- ServerTest.buildServer[R](connectionRef, transmissionRef, forwardQueue, forwardStream).use(_ => IO.never).as(ExitCode.Success)
+      code <- ServerTest.buildServer[R](
+        connectionRef,
+        transmissionRef,
+        forwardQueue,
+        forwardStream,
+        backwardQueue,
+        backwardStream
+      ).use(_ => IO.never).as(ExitCode.Success)
     } yield code
 }
